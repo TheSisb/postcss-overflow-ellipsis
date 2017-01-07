543d602067c7d99ca739d4665d95f64a7d1f1535
TheSisb <shadiisber@gmail.com>
2017-01-06 22:39:41 -0800

    uninline fn

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-overflow-ellipsis', () => {
     const propertyMatch = /^overflow$/;
 
-    return function (css) {
-        css.walkDecls(propertyMatch, decl => {
-            if (decl.value !== 'ellipsis') return;
+    function ruleHandler(decl) {
+        if (decl.value !== 'ellipsis') return;
 
-            decl.cloneBefore({
-                prop: 'white-space',
-                value: 'nowrap'
-            });
-            decl.cloneBefore({
-                prop: 'text-overflow',
-                value: 'ellipsis'
-            });
-
-            decl.value = 'hidden';
+        decl.cloneBefore({
+            prop: 'white-space',
+            value: 'nowrap'
         });
+        decl.cloneBefore({
+            prop: 'text-overflow',
+            value: 'ellipsis'
+        });
+
+        decl.value = 'hidden';
+    }
+
+    return function (css) {
+        css.walkDecls(propertyMatch, ruleHandler);
     };
 });
